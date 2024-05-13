f86755fe528771e248e513a4fb6c2b6e3567f16a
Imastara <sazizimastara@gmail.com>
2024-05-13 21:05:08 +0700

    added example page

---
+++ src/pages/main/Product/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+
+const Product = () => (
+  <div>
+    <h1>Product Page</h1>
+  </div>
+);
+
+export default Product;
---
+++ src/pages/main/Profile/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.css";
+
+const Profile = () => (
+  <div>
+    <h1>Profile Page</h1>
+  </div>
+);
+
+export default Profile;
